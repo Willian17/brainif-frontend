65d180041570e3bb747621951a75f2e8231ff60f
Willian17 <willianfernandes113@gmail.com>
2020-08-10 16:24:15 -0400

    url

--- src/global.js
+++ src/global.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 
 export const userKey = '__brainlif_user'
-export const baseApiUrl = 'http://localhost:3001'
+export const baseApiUrl = 'https://backend-bra.herokuapp.com'
+
 export function showError(e){
     if(e.response.data){
         Vue.toasted.global.msgError({msg: e.response.data})
